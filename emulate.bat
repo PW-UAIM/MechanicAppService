80e7cb878cbcae5026299b677bea5d07f61339d4
K3rne1 <mtjurczak@gmail.com>
2023-04-26 01:54:24 +0200

    Tests fixed

--- emulate.bat
+++ emulate.bat
@@ -2,22 +2,22 @@ echo off
 set url=https://localhost:5011
 
 echo "Mechanik loguje sie swoim identyfikatorem"
-CALL:curl_test GET /mechanic/1/login 
+CALL:curl_test GET /login/1
 echo "Widzi w panelu wszystkie auta"
-CALL:curl_test GET /car/all
+CALL:curl_test GET /getAllCars
 echo "Moze zobaczyc szczegoly konkretnego auta"
-CALL:curl_test GET /car/1
-CALL:curl_test GET /visit/1
+CALL:curl_test GET /getCar/1
+CALL:curl_test GET /getVisit/1
 echo "Moze zmienic status wizyty na naprawione"
-CALL:curl_test PATCH /visit/1/update/W_trakcie
+CALL:curl_test PATCH /updateVisitStatus/1/W_trakcie
 echo:
 echo:
-echo "A nastepnie sprawdzic czy stasus sie zmienil"
-CALL:curl_test GET /visit/1
+echo "A nastepnie sprawdzic czy status sie zmienil"
+CALL:curl_test GET /getVisit/1
 echo "A takze sprawdzic swoj grafik na konkretny dzien"
-CALL:curl_test GET /visit/mechanic/1/date/2022/3/1
+CALL:curl_test GET /getAllVisitsByMechanicInDay/1/2022/3/1
 echo "Albo wszystkie dni, w ktore pracuje"
-CALL:curl_test GET /visit/mechanic/1
+CALL:curl_test GET /getAllVisitsByMechanic/1
 
 EXIT /B 0
 
--- test.bat
+++ test.bat
@@ -1,25 +1,25 @@
 echo off
 set url=https://localhost:5011
 
-CALL:curl_test "Poprawne logowanie" GET /mechanic/1/login 
+CALL:curl_test "Poprawne logowanie" GET /login/1
 
-CALL:curl_test "Niepoprawne logowanie" GET /mechanic/2000/login
+CALL:curl_test "Niepoprawne logowanie" GET /login/2000
 
-CALL:curl_test "Dane wizyty nr 1" GET /visit/1
+CALL:curl_test "Dane wizyty nr 1" GET /getVisit/1
 
-CALL:curl_test "Zakoncz wizyte" PATCH /visit/1/update/Naprawiono
+CALL:curl_test "Zakoncz wizyte" PATCH /updateVisitStatus/1/Naprawiono
 
-CALL:curl_test "Dane wizyty nr 1" GET /visit/1
+CALL:curl_test "Dane wizyty nr 1" GET /getVisit/1
 
-CALL:curl_test "Wizyty mechanika o ID 1" GET /visit/mechanic/1
+CALL:curl_test "Wizyty mechanika o ID 1" GET /getAllVisitsByMechanic/1
 
-CALL:curl_test "Wizyty mechanika o ID 1 w dniu 01.01.2023" GET /visit/mechanic/1/date/2022/3/1
+CALL:curl_test "Wizyty mechanika o ID 1 w dniu 01.01.2023" GET /getAllVisitsByMechanicInDay/1/2022/3/1
 
-CALL:curl_test "Wszystkie auta" GET /car/all
+CALL:curl_test "Wszystkie auta" GET /getAllCars
 
-CALL:curl_test "Dane auta nr 1" GET /car/1
+CALL:curl_test "Dane auta nr 1" GET /getCar/1
 
-CALL:curl_test "Dane wizyty nr 1" GET /visit/1
+CALL:curl_test "Dane wizyty nr 1" GET /getVisit/1
 
 EXIT /B 0
 
